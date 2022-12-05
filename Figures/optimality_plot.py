079a66b5a2f05152cc50cd9f5421802454b3a939
polinagaindrik <polinagayndrik@gmail.com>
2022-12-05 17:58:56 +0200

    optimality criteria: change geometrical interpretation for A criteria and correct the shape of the figure

--- Figures/optimality_plot.py
+++ Figures/optimality_plot.py
@@ -43,7 +43,7 @@ a=2.     #radius on the x-axis
 b=1.    #radius on the y-axis
 alpha=0.1 * 2.0*np.pi #rotation angle
 
-t = np.linspace(0, 2*np.pi, 200)
+t = np.linspace(0, 2*np.pi, 300)
 x, y = ellipse(t, u, v, a, b, alpha)
 xr, yr = rectangle(t, u, v, a, b, alpha)
 
@@ -55,10 +55,10 @@ id_min = np.argmin((u-x)**2 + (v-y)**2)
 beta = np.arctan(b/a)
 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(6, 6))
 ax.fill(x, y, color=mcolors.CSS4_COLORS['lightsteelblue'])
 ax.plot(x, y, color=mcolors.CSS4_COLORS['midnightblue'])
-ax.plot(xr, yr, ":", color=mcolors.CSS4_COLORS['gray'])
+ax.plot(xr, yr, "--", color=mcolors.CSS4_COLORS['gray'])
 
 # Create a rectangle in which ellipsoid is located
 dxa = np.cos(alpha+beta)*np.sqrt(a**2 + b**2)
@@ -70,14 +70,14 @@ dyr = np.sin(0.25*2.0*np.pi-alpha+beta)*np.sqrt(a**2 + b**2)
 # Plot the arrows at the minimum and maximum distance
 ax.arrow(u, v, x[id_max]-u, y[id_max]-v, shape="full", color=mcolors.CSS4_COLORS['black'])
 ax.arrow(u, v, x[id_min]-u, y[id_min]-v, color=mcolors.CSS4_COLORS['black'])
-ax.arrow(u, v, dxa, dya, color=mcolors.CSS4_COLORS['black'])
+#ax.arrow(u, v, dxa, dya, color=mcolors.CSS4_COLORS['black'])
 #ax.text(u+0.25, v, "E-optimality", rotation=30)
 ax.text(u+0.6, v+0.1, r"$\lambda_1$", fontsize=20)
 ax.text(u-0.5, v+0.15, r"$\lambda_2$", fontsize=20)
 
 
-ax.set_xlim(np.min(x)-0.7, np.max(x)+0.7)
-ax.set_ylim(np.min(x)-0.7, np.max(y)+0.7)
+ax.set_xlim(np.min(xr)-0.15, np.max(xr)+0.15)
+ax.set_ylim(np.min(xr)-0.15, np.max(yr)+0.15)
 ax.set_xlabel(r'$p_2$', fontsize=20)
 ax.set_ylabel(r'$p_1$', fontsize=20, rotation=90)
 frame1 = plt.gca()
